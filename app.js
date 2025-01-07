526a3a3153615d5778884c553eb391d9c08bcb54
VioniWijaya <viowijayaputri@gmail.com>
2025-01-07 20:38:46 +0700

    ui login admin user done

--- app.js
+++ app.js
@@ -1,11 +1,44 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+require('dotenv').config();
 
-app.get('/', (req, res) => {
-  res.send('Hello Sengg')
-})
+// const indexRouter = require('./routes/index');
+const authRouter = require('./routes/authroute');
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+const app = express();
+
+// View engine setup
+app.set('views', path.join(__dirname, 'view'));
+app.set('view engine', 'ejs');
+
+// Middleware
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use('/auth', authRouter);
+
+// Error handling
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+app.use((err, req, res) => {
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    res.status(err.status || 500);
+    res.render('error');
+});
+
+module.exports = app;
+
+
+
+
--- routes/authroute.js
+++ routes/authroute.js
@@ -1,1 +1,11 @@
-// router login dan register
+var express = require('express');
+var router = express.Router();
+
+router.get('/user', (req, res) => {
+    res.render('user/login');
+});
+router.get('/admin', (req, res) => {
+    res.render('admin/loginAdmin');
+});
+
+module.exports = router;
