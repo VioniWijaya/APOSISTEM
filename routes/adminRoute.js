644d325d5cc3e0d1fa124b76cf7f86136f29c418
unknown <anastasyaestuwahyudii@gmail.com>
2025-01-09 16:00:00 +0700

    oke

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/dashboard', (req, res) => {
-    res.render('admin/beranda');
+    res.render('admin/dashboard');
 });
 
 router.get('/profil', (req, res) => {
